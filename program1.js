beee68797d433d73644be9d276f29a95066c51eb
Rishikesh kumar <kumarrishikesh023@gmail.com>
2024-09-25 15:05:49 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -23,7 +23,6 @@ var isValid = function (s) {
   return stack.length===0;
 };
 console.log(isValid("()"));
-console.log(isValid("()()"));
 console.log(isValid("(]"));
 console.log(isValid("([)]"));
 
