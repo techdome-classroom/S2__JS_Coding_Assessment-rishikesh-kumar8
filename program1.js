8dcd5e22299337e998848669f618800d0af67b4f
Rishikesh kumar <kumarrishikesh023@gmail.com>
2024-09-25 14:55:00 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,28 +2,24 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    let stack =[];
-    let map ={
-        ')':'(',
-        ']':'[',
-        '}':'{'
-
-    };
-    for (let i=0;i<s.length;i++){
-        let char =s[i];
-        if(char in map ){
-            let topElement=stack.length===0?'#':
-            stack.pop();
-            if(map[char]!==topElement){
-                return false;
-            }else{
-                stack.push(char);
-            }
-        }
+var isValid = function (s) {
+  let stack = [];
+  let map = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char in map) {
+      let topElement = stack.length === 0 ? "#" : stack.pop();
+      if (map[char] !== topElement) {
+        return false;
+      } else {
+        stack.push(char);
+      }
     }
+  }
 };
 
 module.exports = { isValid };
-
-
