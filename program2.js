e35d60adf3f74085048b78cb83817cd5d6a375e2
Rishikesh kumar <kumarrishikesh023@gmail.com>
2024-09-25 15:17:27 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -14,7 +14,7 @@ var romanToInt = function(s) {
     }
     let total=0;
     for(let i=0;i<s.length;i++){
-        const curr=map
+        const curr=map[s[i]]
         if(m[s[i]]<m[s[i]]){
             ans-=m[s[i]];
         }else{
